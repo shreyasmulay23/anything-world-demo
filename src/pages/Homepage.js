67e2b671792f8aa1790f564f9474b6f4aecc95f2
Shreyas Mulay <shreyasmulay23@gmail.com>
2022-05-03 18:55:27 +0530

    Init files.

---
+++ src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NameDescription from "../components/NameDescription";
+import NameSideBar from "../components/NameSideBar";
+
+const Homepage = () => {
+  return (
+    <>
+      <NameSideBar />
+      <NameDescription />
+    </>
+  );
+};
+
+export default Homepage;
